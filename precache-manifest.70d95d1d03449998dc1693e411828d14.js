4fcef60f2761c35850bf678bc879a6c83f5015df
Arvid Peldan <peldans@gmail.com>
2020-04-09 23:11:27 +0200

    Updates

---
+++ precache-manifest.70d95d1d03449998dc1693e411828d14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6939e6ac58c5a604b66c80f8097bd51a",
+    "url": "/homepage-fe/index.html"
+  },
+  {
+    "revision": "d0a4de24f43e3dc957bf",
+    "url": "/homepage-fe/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "924cf1b4f63ada088b24",
+    "url": "/homepage-fe/static/css/main.7f1d7042.chunk.css"
+  },
+  {
+    "revision": "d0a4de24f43e3dc957bf",
+    "url": "/homepage-fe/static/js/2.ea64acac.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/homepage-fe/static/js/2.ea64acac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "924cf1b4f63ada088b24",
+    "url": "/homepage-fe/static/js/main.71dfb43f.chunk.js"
+  },
+  {
+    "revision": "3de5d79ec5a18c534bdd",
+    "url": "/homepage-fe/static/js/runtime-main.f4a32649.js"
+  }
+]);
